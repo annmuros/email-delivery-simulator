221377000c71823ed33e1986fe2d618b88c738c8
annmuros <hello@annamuros.com>
2023-01-30 16:10:55 +0100

    update

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -10,7 +11,25 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar (e){
-        console.log(e.target.value);
+        console.log(e.target.parentElement)
+        if(e.target.value.trim() === '') {
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            //console.log('está vacio');
+        }else{
+            console.log('si hay algo...');
+        }
+    }
+
+    function mostrarAlerta(mensaje, referencia){
+        //generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        //console.log(error);
+
+        //inyectar el error al formulario
+        referencia.appendChild(error);
+
     }
 
 });
