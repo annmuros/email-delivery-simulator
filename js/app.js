1c2e08ca411eb672d7b376756d237f180f35b478
annmuros <hello@annamuros.com>
2023-01-30 17:18:23 +0100

    validate email with regular expression

--- js/app.js
+++ js/app.js
@@ -11,16 +11,24 @@ document.addEventListener('DOMContentLoaded', function(){
     inputMensaje.addEventListener('blur', validar);
 
     function validar (e){
-        console.log(e.target.parentElement)
+        //console.log(e.target.parentElement)
         if(e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            //console.log('está vacio');
-        }else{
-            console.log('si hay algo...');
+            return;
+            
         }
+                            //estas dos condiciones se han de cumplir-tiene que ser un email y tiene que fallar el email
+        if (e.target.id==='email' && !validarEmail(e.target.value)){
+            mostrarAlerta ('el email no es válido', e.target.parentElement )
+            return;
+
+        }
+        limpiarAlerta(e.target.parentElement);//elimina solo la alerta que toca
     }
 
     function mostrarAlerta(mensaje, referencia){
+        limpiarAlerta(referencia );
+
         //generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
@@ -30,6 +38,19 @@ document.addEventListener('DOMContentLoaded', function(){
         //inyectar el error al formulario
         referencia.appendChild(error);
 
+    }
+    function limpiarAlerta(referencia) {
+         //Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if (alerta){
+             alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ //expresión regular usuario@dominio.com
+        const resultado =regex.test(email);
+        return resultado;
     }
 
 });
